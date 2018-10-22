b7e08562d6beb927619d146fe08bdfc103f6540d
David Feinzimer <dfeinzimer@gmail.com>
2018-10-22 16:26:02 -0700

    0.0.3

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import pygame
 from settings import Settings
 
 settings = Settings()
-pygame.display.set_mode((settings.screen_width, settings.screen_height))
-pygame.display.set_caption(settings.game_title)
 
 
 class Game:
     def __init__(self):
         pygame.init()
+        pygame.display.set_caption(settings.game_title)
+        self.screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
 
     def __str__(self):
         return settings.game_title
 
     def play(self):
         while settings.game_title:
+            self.screen.fill(settings.game_color)
             pygame.display.flip()
 
 
--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 class Settings:
     def __init__(self):
         self.game_active = True
+        self.game_color = (119, 108, 255)
         self.game_title = "Super Mario"
         self.screen_height = 720
         self.screen_width = 1280
