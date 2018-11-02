debbc996258c06be7a11661959c14e016c0fc0c7
bobberino98 <bobberino98@gmail.com>
2018-11-02 09:23:33 -0700

    mario spritesheet updated

--- mario.py
+++ mario.py
@@ -4,6 +4,8 @@ from gravity import Gravity
 
 
 class Mario(Sprite):
+    SPEED_CAP = 50
+    ACCEl_FACTOR = 5
     def __init__(self, screen, settings):
         super(Mario, self).__init__()
         self.screen = screen
@@ -11,6 +13,10 @@ class Mario(Sprite):
         self.gravity = Gravity()
         self.image = pygame.image.load("media/images/mario/regular.png")
         self.rect = self.image.get_rect()
+        self.moving_right = False
+        self.moving_left = False
+        self.speed = 0
+
 
     def __str__(self):
         return 'Mario: x:' + str(self.rect.x) + ' y:' + str(self.rect.y)
@@ -18,6 +24,10 @@ class Mario(Sprite):
     def update(self, gamemap):  # Update and then blit
         if not gamemap.object_hit_ground(self):
             self.gravity.perform(self)
+
+        if self.moving_right:
+
+
         self.blitme()
 
     def blitme(self):   # Blit Mario
--- user_control.py
+++ user_control.py
@@ -2,15 +2,49 @@ import pygame
 import sys
 
 
-def check_events():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print("EXITING")
+class Controller:
+    def __init__(self, mario):
+        self.mario = mario
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("EXITING")
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                print("KEY DOWN")
+                self.check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                print("KEY UP")
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                print("CLICK AT", mouse_x, mouse_y)
+
+    def check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.mario.moving_right = True
+            self.mario.moving_left = False
+            self.mario.moving_up = False
+            self.mario.moving_down = False
+        elif event.key == pygame.K_LEFT:
+            self.mario.moving_left = True
+            self.mario.moving_right = False
+            self.mario.moving_up = False
+            self.mario.moving_down = False
+        elif event.key == pygame.K_UP:
+            self.mario.moving_up = True
+            self.mario.moving_left = False
+            self.mario.moving_down = False
+            self.mario.moving_right = False
+
+        elif event.key == pygame.K_DOWN:
+            self.mario.moving_down = True
+            self.mario.moving_left = False
+            self.mario.moving_right = False
+            self.mario.moving_up = False
+        elif event.key == pygame.K_q:
+            self.mario.create_portal(1)
+        elif event.key == pygame.K_e:
+            self.mario.create_portal(2)
+        elif event.key == pygame.K_ESCAPE:
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            print("KEY DOWN")
-        elif event.type == pygame.KEYUP:
-            print("KEY UP")
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            print("CLICK AT", mouse_x, mouse_y)
