67c8ac8989f599f056ab655aa4cd1ec522865fe3
David Feinzimer <dfeinzimer@gmail.com>
2018-11-03 00:05:50 -0700

    21.00.11.2.2018

--- map.py
+++ map.py
@@ -1,6 +1,6 @@
 from pygame.sprite import Group
 import pygame
-import brick
+from brick import *
 
 
 class Map:
@@ -11,7 +11,7 @@ class Map:
         self.settings = settings
         self.screen = screen
         self.dist = 0
-        self.gapcols = [70, 71, 87, 88, 89, 156, 157]
+        self.gapcols = [70, 71, 87, 88, 89, 156, 157]  # Gap columns
         self.b3cols = [21, 23, 25, 78, 80, 95, 101, 102, 119, 130, 131, 171, 172, 174]
         self.b7cols = [81, 82, 83, 84, 85, 86, 87, 88, 92, 93, 94, 122, 123, 124, 129, 132]
         self.q7cols = [23, 95, 110, 130, 131]
@@ -19,38 +19,40 @@ class Map:
         self.lpipe1cols = [29]
         self.rpipe1cols = [30]
 
-        for num in range(224):  # start with 18 columns of bricks
+        for num in range(224):  # Build each column
             self.add_column(num)
 
-    def add_column(self, num):  # create a new column of bricks
-        if num not in self.gapcols:
+    def add_column(self, num):  # Create a new column of bricks
+        if num in self.gapcols:
+            pass
+        else:
             new_column = Group()
             if len(self.brick_columns) == 0:
                 x = 0
             else:
                 x = 32 * num
             for i in range(2):
-                brick_temp = brick.Brick(self.screen)
+                brick_temp = Brick(self.screen)
                 brick_temp.rect.x = x
                 brick_temp.rect.y = self.settings.brick_y_offset + (32 * i)
                 new_column.add(brick_temp)
                 if num in self.b3cols:
-                    brick_temp = brick.BreakBrick(self.screen)
+                    brick_temp = BreakBrick(self.screen)
                     brick_temp.rect.x = x
                     brick_temp.rect.y = self.settings.brick_y_offset - 96
                     new_column.add(brick_temp)
                 if num in self.q3cols:
-                    brick_temp = brick.QBrick(self.screen)
+                    brick_temp = QBrick(self.screen)
                     brick_temp.rect.x = x
                     brick_temp.rect.y = self.settings.brick_y_offset - 96
                     new_column.add(brick_temp)
                 if num in self.q7cols:
-                    brick_temp = brick.QBrick(self.screen)
+                    brick_temp = QBrick(self.screen)
                     brick_temp.rect.x = x
                     brick_temp.rect.y = self.settings.brick_y_offset - 32*7
                     new_column.add(brick_temp)
                 if num in self.b7cols:
-                    brick_temp = brick.BreakBrick(self.screen)
+                    brick_temp = BreakBrick(self.screen)
                     brick_temp.rect.x = x
                     brick_temp.rect.y = self.settings.brick_y_offset - 32 * 7
                     new_column.add(brick_temp)
@@ -72,9 +74,9 @@ class Map:
                 x.im_rect.blitme()
 
     def object_hit_ground(self, item):
-        for x in self.brick_columns:
-            if pygame.sprite.spritecollideany(item, x):
-                print(x)
-                #if item.rect.bottom == x.at(0).rect.top:
-                print(item.__str__() + " is touching the ground")
-                return True
+        for column in self.brick_columns:
+                for brick in column:
+                    if pygame.sprite.collide_rect(item, brick):
+                        if abs(brick.rect.top - item.rect.bottom) <= 10:
+                            # print(item.__str__() + " is touching the ground")
+                            return True
--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 class Settings:
     def __init__(self):
-        self.brick_y_offset = 384  # determines how low bricks appear on the screen
+        self.brick_y_offset = 416  # determines how low bricks appear on the screen
         self.color_mario_blue = (119, 108, 255)  # classic blue color
         self.game_title = "Super Mario"
-        self.screen_height = 448
+        self.screen_height = 480
         self.screen_width = 1152
