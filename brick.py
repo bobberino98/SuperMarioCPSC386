8bed5f2b420e11e40a95ff982b682142143e25a1
David Feinzimer <dfeinzimer@gmail.com>
2018-11-02 20:15:23 -0700

    15.47.11.2.2018

--- brick.py
+++ brick.py
@@ -1,32 +1,31 @@
-import pygame
 from pygame.sprite import Sprite
 from imagerect import ImageRect
 
 
-class Brick(Sprite):
+class Brick(Sprite):  # Floor bricks
     def __init__(self, screen):
         super(Brick, self).__init__()
         self.screen = screen
-        self.im_rect = ImageRect(screen, "media/images/mario_brick", 32, 32)
+        self.im_rect = ImageRect(screen, "media/images/brick/floor", 32, 32)
         self.rect = self.im_rect.rect
 
     def __str__(self):
         return 'Brick: x:' + str(self.rect.x) + ' y:' + str(self.rect.y)
 
 
-class QBrick(Sprite):
+class QBrick(Sprite):  # Question mark bricks
     def __init__(self, screen):
         super(QBrick, self).__init__()
         self.screen = screen
-        self.im_rect = ImageRect(screen, "media/images/q_brick", 32, 32)
+        self.im_rect = ImageRect(screen, "media/images/brick/question", 32, 32)
 
         self.rect = self.im_rect.rect
 
 
-class BreakBrick(Sprite):
+class BreakBrick(Sprite):  # Floating bricks
     def __init__(self, screen):
         super(BreakBrick, self).__init__()
         self.screen = screen
-        self.im_rect = ImageRect(screen, "media/images/float_brick", 32, 32)
+        self.im_rect = ImageRect(screen, "media/images/brick/platform", 32, 32)
 
         self.rect = self.im_rect.rect
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from user_control import Controller
 from map import Map
 from mario import Mario
 from imagerect import ImageRect
+
+
 settings = Settings()
 
 
@@ -14,9 +16,9 @@ class Game:
         pygame.display.set_caption(settings.game_title)
         self.screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
         self.map = Map(self.screen, settings)
-        self.background = ImageRect(self.screen, "media/background", settings.screen_width, settings.screen_height)
-        self.background.rect.left = 0
-        self.background.rect.top = 0
+        # self.background = ImageRect(self.screen, "media/background", settings.screen_width, settings.screen_height)
+        # self.background.rect.left = 0
+        # self.background.rect.top = 0
         self.mario = Mario(self.screen, settings, self.map)
 
     def __str__(self):
@@ -28,7 +30,7 @@ class Game:
         user_control = Controller(self.mario)
         while True:
             self.screen.fill(settings.color_mario_blue)
-            self.background.blitme()
+            # self.background.blitme()
             user_control.check_events()
             self.map.update()
             self.mario.update(self.map)
